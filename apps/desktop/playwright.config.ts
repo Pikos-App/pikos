fb94656cec9ab671b76e5e0cd1e73b6dee0f40c5
alex <hello@alex-ak.com>
2026-03-28 14:12:14 -0700

    CI refinement

--- apps/desktop/playwright.config.ts
+++ apps/desktop/playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env["CI"],
   retries: process.env["CI"] ? 2 : 0,
-  workers: process.env["CI"] ? "75%" : undefined,
+  workers: process.env["CI"] ? 2 : undefined,
   reporter: [["list"], ["html", { open: "never" }]],
   use: {
     baseURL: "http://localhost:1420",
